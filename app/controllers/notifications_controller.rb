44bd6b4cb4f46d2742151157252ce068ed9a8980
vincemilo <vincemilo@gmail.com>
2022-10-23 19:15:28 -0600

    cleaned up notifs index logic and put in controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,6 +3,18 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = Notification.where('user_id = ?', current_user.id)
-    
+    return if @notifications.blank?
+
+    @requests = []
+
+    @notifications.each do |notification|
+      if notification.notice_type == 'request'
+        @requests << User.find(notification.notice_id)
+      end
+    end
+
+    # @requests.each do |request|
+    #   @users << User.find(request.notice_id)
+    # end
   end
 end
