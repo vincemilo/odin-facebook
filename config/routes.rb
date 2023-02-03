3d96f24c4b547ce45d5d432b1c0a493d72e89bb9
vincemilo <vincemilo@gmail.com>
2023-02-03 10:49:40 -0700

    added omniauth for fb but might need to be deployed to work as im getting Cant load URL error on localhost

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: %i[index show] do
     resources :friendships, only: %i[create update destroy]
   end
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   end
   resources :notifications
   resources :likes
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
