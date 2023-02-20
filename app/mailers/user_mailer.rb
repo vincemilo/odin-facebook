e5569e4e0a1bdfde38a0440d5fd90a96e9d893b3
vincemilo <vincemilo@gmail.com>
2023-02-19 20:06:26 -0700

    updated devise files to fix error with help from carlosantoniodasilva

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'phunbaba@protonmail.com'
 
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Odin Facebook')
   end
 end
